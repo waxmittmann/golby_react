4b48be183ba5a65685cd561a6cf61933a0db918e
Maximilian Wittmann <Maximilian.Wittmann@cba.com.au>
2016-05-09 16:24:04 +1000

    Fixed the overwriting thing, but only with a huge mess in reducers.js that removes the es2015 goodness

--- src/reducers.js
+++ src/reducers.js
@@ -41,7 +41,25 @@ function visibilityFilter(state = VisibilityFilters.ALL, action) {
 function blogposts(state = {}, action) {
   switch(action.type) {
     case ADD_POST:
-      //const newId = Object.keys(state).length
+      const newId = (Object.keys(state).length + 1)
+      console.log("For state:\n")
+      console.log(state)
+      console.log("Assigning with " + newId)
+      var result =  Object.assign({}, state)
+      console.log("Copy is:\n")
+      console.log(result)
+      result[newId] = {
+        //id: newId,
+        id: 1,
+        body: action.body,
+        title: action.title,
+        published: false
+      }
+      console.log("After it is:\n")
+      console.log(result)
+
+      return result
+      /*
       return Object.assign({}, {
           ...state,
           newId: {
@@ -51,7 +69,7 @@ function blogposts(state = {}, action) {
             title: action.title,
             published: false
           }
-        })
+        })*/
 
     case PUBLISH_POST:
         state.map((post) => {
